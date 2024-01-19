47dc3e45ef54769777475afc1b2f8b0c4072afba
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-01-19 16:56:15 -0300

    fix: remove author repository declaration at books repository

--- src/infrastructure/database/sqlalchemy/repositories/books.py
+++ src/infrastructure/database/sqlalchemy/repositories/books.py
@@ -3,37 +3,12 @@ from typing import List
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.exc import NoResultFound
 
-from domain.entities.author import Author as AuthorEntity
 from domain.entities.book import Book as BookEntity
-from infrastructure.database.sqlalchemy.mappers.author import AuthorMapper
 from infrastructure.database.sqlalchemy.mappers.book import BookMapper
 from infrastructure.database.sqlalchemy.models.author import (
     Author as AuthorModel,
 )
 from infrastructure.database.sqlalchemy.models.book import Book
-
-
-class AuthorRepository:
-    def __init__(self, db: Session):
-        self.db = db
-
-    def get_author_by_name(self, name: str) -> AuthorEntity:
-        author = self.db.query(AuthorModel).filter_by(name=name).first()
-        if author:
-            return AuthorMapper.model_to_entity(author)
-        else:
-            return None
-
-    def create_author(self, name: str) -> AuthorEntity:
-        try:
-            author = AuthorModel(name=name)
-            self.db.add(author)
-            self.db.commit()
-            self.db.refresh(author)
-            return AuthorMapper.model_to_entity(author)
-        except Exception as error:
-            self.db.rollback()
-            raise error
 
 
 class BookRepository:
