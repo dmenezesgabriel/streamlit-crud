26af06a24ba842d8e19ac858f53c1cf4fca9f718
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-02-11 15:20:28 -0300

    fix: database creation script

--- scripts/database.py
+++ scripts/database.py
@@ -1,3 +1,4 @@
+import asyncio
 import sys
 
 sys.path.append("src")
@@ -7,4 +8,12 @@ from external.database.sqlalchemy.models.book import Book
 from external.database.sqlalchemy.models.event import Event
 from external.database.sqlalchemy.orm import Base, engine
 
-Base.metadata.create_all(bind=engine)
+
+async def main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
