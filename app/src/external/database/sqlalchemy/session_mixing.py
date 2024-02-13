6bb63e84b1c8f1ee333bc2401ba806f9da152d97
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-02-13 20:29:47 -0300

    feat: add type hints to DatabaseSessionMixin context exit

--- app/src/external/database/sqlalchemy/session_mixing.py
+++ app/src/external/database/sqlalchemy/session_mixing.py
@@ -1,3 +1,5 @@
+from typing import Any, Optional, Tuple, Type
+
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.external.database.sqlalchemy.orm import async_session
@@ -10,7 +12,12 @@ class DatabaseSessionMixin:
         self.session = async_session()
         return self.session
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[Any],
+    ) -> None:
         try:
             if exc_type is not None:
                 await self.session.rollback()
